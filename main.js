be9989fda185a9dff3b2eaacde6d366857a5544f
joshwill221 <joshwill221@gmail.com>
2017-05-29 16:59:33 +0800

    Working prototype complete

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ var mainState = {
         // This function will be executed at the beginning
         // That's where we load the images and sounds
         
-        // Load the bird sprite
+        // Load sprites
         game.load.image('bird', 'assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
     },
     
     create: function() {
@@ -31,6 +32,19 @@ var mainState = {
         // Call the 'jump' function when the spacekey is hit
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
+        
+        /* Pipes */
+        
+        // Create an empty group
+        this.pipes = game.add.group();
+        
+        // Spawn pipes every 1.5s
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        
+        /* Score */
+        
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
     },
     
     update: function() {
@@ -41,6 +55,8 @@ var mainState = {
         // Call the 'restartGame function
         if (this.bird.y < 0 || this.bird.y > 490)
             this.restartGame();
+        
+        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
     },
     
     // Make the bird jump
@@ -53,7 +69,44 @@ var mainState = {
     restartGame: function() {
         // Start the 'main' state, which restarts the game
         game.state.start('main');
-    }
+    },
+    
+    // Add a pipe at the parameter coordinates
+    addOnePipe: function(x, y) {
+        // Create a pipe at the position x and y
+        var pipe = game.add.sprite(x, y, 'pipe');
+        
+        // Add the pipe to our previously created group
+        this.pipes.add(pipe);
+        
+        // Enable physics on the pipe
+        game.physics.arcade.enable(pipe);
+        
+        // Add velocity to the pipe to make it move left
+        pipe.body.velocity.x = -200;
+        
+        // Automatically kill the pipe when it's no longer visible
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+    
+    addRowOfPipes: function() {
+        // Randomly pick a number between 1 and 5
+        // This will be the hole position
+        var hole = Math.floor(Math.random() * 5) + 1;
+        
+        // Add the 6 pipes
+        // With one big hole at position 'hole' and 'hole + 1'
+        for (var i = 0; i < 8; i++)
+            if (i != hole && i != hole + 1)
+                this.addOnePipe(400, i * 60 + 10);
+        
+        // Top or Bottom cannot be a hole
+        // This means there is half the chance of 2nd top or 2nd bottom being a hole
+        
+        this.score += 1;
+        this.labelScore.text = this.score;
+    },
 };
 
 // Initialize Phaser, and create a 400px by 490px game
